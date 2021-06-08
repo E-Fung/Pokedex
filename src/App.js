606d7b6995535513c12cea24438a25046db72db8
ehhfung <ehhfung@gmail.com>
2021-06-08 13:54:22 -0400

    transitioned to functional components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from "react";
 import "./App.css";
-import PokemonList from "./components/PokemonList";
+import PokemonListFC from "./components/PokemonListFC";
 
 class App extends Component {
   state = {};
   render() {
     return (
       <div>
-        <PokemonList />
+        <PokemonListFC />
       </div>
     );
   }
