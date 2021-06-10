c60571596f9a7375fbc4f53bc798f3a513d3c6c3
ehhfung <ehhfung@gmail.com>
2021-06-10 13:06:43 -0400

    made list of pokemon obj

--- src/components/Pokemon.jsx
+++ src/components/Pokemon.jsx
@@ -1,32 +1,13 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-import PokemonPic from "./PokemonPic";
+import React, { useState } from "react";
+// import PokemonPic from "./PokemonPic";
+// import Evolution from "./Evolution";
 
 //container for individual pokemons
 
 export default function Pokemon(props) {
-  const [pokeState, setPokeState] = useState(null);
-  const [index] = useState(props.index);
-
-  // Similar to componentDidMount and componentDidUpdate:
-  useEffect(() => {
-    let url = `https://pokeapi.co/api/v2/pokemon/${index}`;
-    axios.get(url).then((data) => {
-      setPokeState(data);
-    });
-  }, [index]);
-
-  if (!pokeState) {
-    return <div>Loading...</div>;
-  } else {
-    console.log(pokeState.data);
-    return (
-      <div>
-        <PokemonPic index={index}></PokemonPic>
-        {pokeState.data.name}
-      </div>
-    );
-  }
+  const [pokemon, setPokemon] = useState(props.pokemon);
+  console.log(pokemon);
+  return <div>{pokemon.data.name}</div>;
 }
 
 //Functional Components > Class Components ALWAYS
--- src/components/PokemonList.jsx
+++ src/components/PokemonList.jsx
@@ -1,12 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Pokemon from "./Pokemon";
+import axios from "axios";
 
 export default function PokemonList() {
-  let myArray = Array.from({ length: 7 }, (_, i) => i + 1);
+  const [pokemonList, setPokemonList] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const totalPokemon = 5;
+  let tempArray = new Array(totalPokemon);
+  let tempCounter = totalPokemon;
+
+  useEffect(() => {
+    for (let index = 1; index <= totalPokemon; index++) {
+      let url = `https://pokeapi.co/api/v2/pokemon/${index}`;
+      axios.get(url).then((data) => {
+        //think of this code not running in the initial sequence
+        tempArray[data.data.id - 1] = data;
+        console.log("grabbing data");
+        console.log(data.data.name);
+        tempCounter--;
+        if (!tempCounter) {
+          setPokemonList(tempArray);
+          setLoading(false);
+          console.log("Ready");
+        }
+      });
+    }
+  }, []);
+
+  if (loading) {
+    console.log("loading");
+    return <div>Loading...</div>;
+  }
+  if (!pokemonList) {
+    console.log("still loading");
+    return <div>Still Loading</div>;
+  }
+  console.log("rendering");
+  console.log(pokemonList);
   return (
     <div>
-      {myArray.map((index) => (
-        <Pokemon index={index} key={index}></Pokemon>
+      {pokemonList.map((pokemon) => (
+        <Pokemon pokemon={pokemon}></Pokemon>
       ))}
     </div>
   );
